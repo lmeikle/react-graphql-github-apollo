1e65af0c333301591174fe71ea6b7262e20f56d3
Laura Meikle <laura.s.meikle@gmail.com>
2020-04-24 13:37:48 +0100

    pagination

--- src/Button/index.js
+++ src/Button/index.js
@@ -16,4 +16,17 @@ const Button = ({
     {children}
   </button>
 );
+
+const ButtonUnobtrusive = ({
+  children,
+  className,
+  type = "button",
+  ...props
+}) => (
+  <button className={`${className} Button_unobtrusive`} type={type} {...props}>
+    {children}
+  </button>
+);
+
 export default Button;
+export { ButtonUnobtrusive };
--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,12 +1,48 @@
 import React from "react";
 import RepositoryItem from "../RepositoryItem";
+import FetchMore from "../../FetchMore";
 import "../style.css";
 
-const RepositoryList = ({ repositories }) =>
-  repositories.edges.map(({ node }) => (
-    <div key={node.id} className="RepositoryItem">
-      <RepositoryItem {...node} />
-    </div>
-  ));
+const updateQuery = (previousResult, { fetchMoreResult }) => {
+  if (!fetchMoreResult) {
+    return previousResult;
+  }
+
+  return {
+    ...previousResult,
+    viewer: {
+      ...previousResult.viewer,
+      repositories: {
+        ...previousResult.viewer.repositories,
+        ...fetchMoreResult.viewer.repositories,
+        edges: [
+          ...previousResult.viewer.repositories.edges,
+          ...fetchMoreResult.viewer.repositories.edges,
+        ],
+      },
+    },
+  };
+};
+
+const RepositoryList = ({ repositories, fetchMore, loading }) => (
+  <>
+    {repositories.edges.map(({ node }) => (
+      <div key={node.id} className="RepositoryItem">
+        <RepositoryItem {...node} />
+      </div>
+    ))}
+    <FetchMore
+      loading={loading}
+      hasNextPage={repositories.pageInfo.hasNextPage}
+      variables={{
+        cursor: repositories.pageInfo.endCursor,
+      }}
+      updateQuery={updateQuery}
+      fetchMore={fetchMore}
+    >
+      Repositories
+    </FetchMore>
+  </>
+);
 
 export default RepositoryList;
